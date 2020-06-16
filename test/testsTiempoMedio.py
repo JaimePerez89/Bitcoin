5a5206b63bea56a872d56e059fe9f95ce976b99b
Jaime Perez <ordieres_jaime@hotmail.com>
2020-06-16 21:29:19 +0200

    Añadido TestTiempoMedio y corregido Test Num horario Tran

--- test/testsTiempoMedio.py
+++ test/testsTiempoMedio.py
@@ -1,27 +1,27 @@
 import unittest
 from bitcoin import LecturaInputs
-from bitcoin import NumHorarioTransacciones
+from bitcoin import TiempoMedio
 
 
-class Tam_Horario_Bloques(unittest.TestCase):
+class Tiempo_Medio_Horario(unittest.TestCase):
 
     def test_dict_tam_bloques(self):
         # Comprobamos si las funciones nos retornan un diccionario
         dict_test = {}  # Creo uno vacio que nos sirva de referencia
         # Necesitamos cargar los datos de txs_data
-        txs_data = LecturaInputs.lectura_transacciones()
+        block_data = LecturaInputs.lectura_blocks()
 
-        self.assertEqual(type(NumHorarioTransacciones.cantidad_transacciones_hr(txs_data, 'N')),
+        self.assertEqual(type(TiempoMedio.tiempo_medio_bloques(block_data, 'N')),
                          type(dict_test))
 
     def test_cant_horas(self):
         # Compruebo que el número de horas es de 24
 
         # Necesitamos cargar los datos de txs_data
-        txs_data = LecturaInputs.lectura_transacciones()
+        block_data = LecturaInputs.lectura_blocks()
 
-        self.assertEqual(len(NumHorarioTransacciones.cantidad_transacciones_hr(txs_data, 'N').keys()),
-                         24)
+        self.assertEqual(len(TiempoMedio.tiempo_medio_bloques(block_data, 'N').keys()),
+                         144)
 
 
 if __name__ == '__main__':
